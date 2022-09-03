550fd83979ec3b516b80a515f5096e9d47cc2dde
Mandreol <marcoandreola01@gmail.com>
2022-09-02 21:45:08 -0300

    primera prueba terminada y completa

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const menuButton = document.querySelector("#menu-button");
 const menuList = document.querySelector("#menu");
+const contactMe = document.querySelector("#contactme");
 const comunicacionButton = document.querySelector("#comunicacion-button");
 const escuchaButton = document.querySelector("#escucha-button");
 const agileButton = document.querySelector("#agile-button");
@@ -8,18 +9,26 @@ const comunicacionText = document.querySelector("#comunicacion");
 const escuchaText = document.querySelector("#escucha");
 const agileText = document.querySelector("#agile");
 const tiempoText = document.querySelector("#tiempo");
+let slider = document.querySelector("#slider");
+let sliderSecction = document.querySelectorAll(".slider_section");
+let sliderSecctionLast = sliderSecction[sliderSecction.length - 1];
+const btnLeft = document.querySelector("#slider_btn--left");
+const btnRight = document.querySelector("#slider_btn--rigth");
 
 menuButton.addEventListener("click", menu);
+contactMe.addEventListener("click", contactMeDesplegar);
 comunicacionButton.addEventListener("click", comunicacionDesplegar);
 escuchaButton.addEventListener("click", escuchaDesplegar);
 agileButton.addEventListener("click", agileDesplegar);
 tiempoButton.addEventListener("click", tiempoDesplegar);
-
+btnRight.addEventListener("click", next);
+btnLeft.addEventListener("click", previus);
 function menu() {
-	console.log("click");
 	menuList.classList.toggle("desplegar_menu");
 }
-
+function contactMeDesplegar() {
+	contactMe.children[1].childNodes[3].toggleClass("desplegar");
+}
 function comunicacionDesplegar() {
 	tiempoText.classList.remove("desplegar");
 	agileText.classList.remove("desplegar");
@@ -43,4 +52,25 @@ function tiempoDesplegar() {
 	escuchaText.classList.remove("desplegar");
 	agileText.classList.remove("desplegar");
 	tiempoText.classList.toggle("desplegar");
+}
+function next() {
+	let sliderSecctionFirst = document.querySelectorAll(".slider_section")[0];
+	slider.style.marginLeft = "-200%";
+	slider.style.transition = "all .5s";
+	setTimeout(function () {
+		slider.style.transition = "none";
+		slider.insertAdjacentElement("beforeend", sliderSecctionFirst);
+		slider.style.marginLeft = "-100%";
+	}, 500);
+}
+function previus() {
+	let sliderSecction = document.querySelectorAll(".slider_section");
+	let sliderSecctionLast = sliderSecction[sliderSecction.length - 1];
+	slider.style.marginLeft = "0%";
+	slider.style.transition = "all .5s";
+	setTimeout(function () {
+		slider.style.transition = "none";
+		slider.insertAdjacentElement("afterbegin", sliderSecctionLast);
+		slider.style.marginLeft = "-100%";
+	}, 500);
 }
